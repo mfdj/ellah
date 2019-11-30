b31abc0ba131cb235f784a981b1204ec437621df
Mark Fox <mf@ixel.org>
2019-11-30 01:29:36 -0800

    Use colored-logger and proxy via local log function

--- core/functions/log.bash
+++ core/functions/log.bash
@@ -1,49 +1,7 @@
 #!/bin/bash
 
 log() {
-   local level
-   local color
-   local use_color
-   local color_off='\x1B[0m'
-   local context="(${0##*/})"
-   local grey='\x1B[0;37m'
-
-   (( $# == 0 )) && {
-      echo 'log: expecting at least one argument' >&2
-      return
-   }
-
-   [[ -t 1 ]] && use_color=1 # not in-a-pipe or file-redireciton
-
-   case $1 in
-      debug) level=0;;
-       info) level=1;;
-       warn) level=2;;
-      error) level=3;;
-   esac
-
-   [[ -z $VERBOSE ]] && context=''
-
-   [[ $level ]] && shift || level=0
-
-   [[ $# -gt 0 ]] || {
-      echo 'log: expecting a message' >&2
-      return
-   }
-
-   case $level in
-      0) color_off=;;
-      1) color='\x1B[0;32m';; # Green
-      2) color='\x1B[0;33m';; # Yellow
-      3) color='\x1B[0;31m';; # Red
-   esac
-
-   [[ $level -gt 0 || $VERBOSE ]] && {
-      [[ $use_color ]] && echo -e "${color}${*?}${color_off} ${grey}${context}${color_off}"
-      [[ $use_color ]] || echo "${*?} ${context}"
-   }
-
-   return 0
+   "${ELLAH_ROOT:?}/deps/colored-logger/colored-logger.bash" "$@"
 }
 export -f log
 
