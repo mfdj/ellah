4d22ea308f29395113a2ba0c5895b1c0bc7db7fd
Mark Fox <mf@ixel.org>
2020-01-05 22:08:36 -0800

    Migrate modules/select-php-version to ellah-kit

--- modules/select-php-version.sh
+++ modules/select-php-version.sh
@@ -1,27 +1,35 @@
+#!/usr/bin/env ellah-kit
+# shellcheck shell=bash
 
-use 'path-helpers'
+use 'core/functions/log'
+use 'functions/demand'
+use 'functions/ensure_symlink'
 
-php_path_symlink() {
+path_to_php=$1
+link_to=$2
+
+demand 'path_to_php'
+demand 'link_to'
+
+link_bin() {
    log debug "linking $1"
 
    # bin
-   if [[ -f ${env_php_path}/bin/$1 ]]; then
-      ensure_symlink "${env_php_path}"/bin/$1 /usr/local/bin/$1
+   if [[ -f ${path_to_php}/bin/$1 ]]; then
+      ensure_symlink "${path_to_php}/bin/${1}" "$link_to/$1"
 
    # sbin
-   elif [[ -f ${env_php_path}/sbin/$1 ]]; then
-      ensure_symlink "${env_php_path}"/sbin/$1 /usr/local/bin/$1
+   elif [[ -f ${path_to_php}/sbin/$1 ]]; then
+      ensure_symlink "${path_to_php}/sbin/${1}" "$link_to/$1"
 
    else
       log warn "skipping: $1 wasn't compiled"
    fi
 }
 
-php_path_symlink php
-php_path_symlink php-fpm
-php_path_symlink pecl
-php_path_symlink phpize
-php_path_symlink pear
-php_path_symlink phar
-
-:
+link_bin php
+link_bin php-fpm
+link_bin pecl
+link_bin phpize
+link_bin pear
+link_bin phar
